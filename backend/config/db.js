c86a8d3bea0f0b55f2183ab643e6eb3cbe502242
Naveena <naveenatm27@gmail.com>
2026-06-16 15:01:22 +0530

    tcommit

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+const connectDB=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected"+process.env.PORT);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+module.exports=connectDB;
